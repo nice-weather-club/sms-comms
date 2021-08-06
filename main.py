b7a5e1617d64b05a2a0e1536945be42ae2620f28
Isaac Keleher <isaac.keleher@gmail.com>
2021-08-06 21:54:32 +1000

    git init

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+@author Isaac Keeher
+This program is an sms forwarder meant to recieve a
+text message and forward it on to multiple numbers
+"""
+
+
+def recieve_sms():
+    pass
+
+
+def send_sms():
+    pass
+
+
+if __name__ == "__main__":
+    print("hello")
