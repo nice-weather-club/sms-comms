1128cbbd8a85fc5830e6708bfd31fe5e7b2cd0f1
Isaac Keleher <isaac.keleher@gmail.com>
2021-08-06 23:17:30 +1000

    wip: connecting to mongodb :)

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ TODO:
 - [ ] add a database for phone numbers
 - [ ] integrate properly with twilio
 - [ ] setup ci/cd
+
+To run you'll need to create a .env file in the top level directory
+e.g.
+```
+EVENT_LOCATION="Westgate Bridge"
+GEO_LOCATION="-37.83184926174339, 144.90473068626446"
+EVENT_TIME="11.30PM"
+
+DB_URL=<URL>
+DB_PW=<SECRET_PASSWORD>
+DB_USER=<USERNAME>
+```
---
+++ db_conn.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+@author Isaac Keeher
+This is a database connector module to fetch
+phone number details from mongoDB atlas
+"""
+
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from pprint import pprint
+
+config = dotenv_values(".env")
+
+DB_PW = config['DB_PW']
+DB_USER = config['DB_USER']
+DB_URL = config['DB_URL']
+
+# connect to MongoDB, you need to fill out the .env for security :)
+client = MongoClient(
+    f"mongodb+srv://{DB_USER}:{DB_PW}@{DB_URL}")
+db=client.admin
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
+
+
